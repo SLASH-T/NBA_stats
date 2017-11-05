4776d9e984d59f3bcbbfc238b22bd60104b315a8
CTLIU <chiatungliu@gmail.com>
2017-11-05 19:39:38 +0800

    modified api_spec.rb

--- spec/api_spec.rb
+++ spec/api_spec.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: false
+
 require_relative 'spec_helper.rb'
 
 describe 'Tests Api functionality' do
@@ -15,22 +17,42 @@ describe 'Tests Api functionality' do
   end
 
   describe 'Game information' do
-    it 'HAPPY: should provide correct game information' do
-      get "#{API_VER}/game_info/#{SEASON}/#{GAMEID}"
-      _(last_response.status).must_equal 200
-      repo_data = JSON.parse last_response.body
-      puts repo_data
-      _(repo_data.size).must_be :>, 0
+    before do
+      # DatabaseCleaner.clean
+      Rake::Task['db:reset'].invoke
     end
 
-=begin
-    it 'SAD: should raise exception on incorrect game insertions' do
-      get "#{API_VER}/game_info/error/#{GAMEID}"
-      _(last_response.status).must_equal 404
-    end
-=end
-  end
+    describe 'POSTting to create entities from Github' do
+      it 'HAPPY: should retrieve and store repo and collaborators' do
+        post "#{API_VER}/game_info/#{SEASON}/#{GAMEID}"
+        _(last_response.status).must_equal 201
+        _(last_response.header['Location'].size).must_be :>, 0
+        game_data = JSON.parse last_response.body
+        _(game_data.size).must_be :>, 0
+      end
 
-  describe 'Contributor information' do
+      it 'SAD: should report error if no Github repo found' do
+        post "#{API_VER}/game_info/#{SEASON}/GGGameId"
+        _(last_response.status).must_equal 404
+      end
+    end
+
+    describe 'GETing database entities' do
+      before do
+        post "#{API_VER}/game_info/#{SEASON}/#{GAMEID}"
+      end
+
+      it 'HAPPY: should find stored game and player stats' do
+        get "#{API_VER}/game_info/#{SEASON}/#{GAMEID}"
+        _(last_response.status).must_equal 200
+        game_data = JSON.parse last_response.body
+        _(game_data.size).must_be :>, 0
+      end
+
+      it 'SAD: should report error if no database game entity found' do
+        get "#{API_VER}/game_info/#{SEASON}/GGGameId"
+        _(last_response.status).must_equal 404
+      end
+    end
   end
 end
